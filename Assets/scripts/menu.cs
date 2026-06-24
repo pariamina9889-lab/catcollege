5cb746527b9b23fbcedcc95ff8b34e51621c8f49
AY <pariamina9889@gmail.com>
2026-06-24 13:41:32 +0330

    menuAdded
    the menu is qorking- lvl3 needs work

---
+++ Assets/scripts/menu.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class menu : MonoBehaviour
+{
+    public Button start;
+    public Button quit;
+    public Button settings;
+    public GameObject settingsPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        start.onClick.AddListener(() => SceneManager.LoadScene("SampleScene"));
+        quit.onClick.AddListener(() => Application.Quit());
+
+        quit.onClick.AddListener(() => {
+          #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+          #else
+             Application.Quit();
+          #endif
+        });
+
+
+        // اطمینان از بسته بودن پنل در ابتدا
+        if (settingsPanel != null)
+            settingsPanel.SetActive(false);
+
+        // اتصال دکمه Settings
+        settings.onClick.AddListener(ShowSettings);
+
+
+        //start.onClick.AddListener(() =>
+        //{
+        //    SceneManager.LoadScene("main");
+        //});
+
+        //quit.onClick.AddListener(() => { 
+        //    Application.Quit();
+        //});
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // اگر کلید Escape زده شد و پنل تنظیمات فعال است
+        if (Input.GetKeyDown(KeyCode.Escape) && settingsPanel != null && settingsPanel.activeSelf)
+        {
+            settingsPanel.SetActive(false); // بستن پنل
+        }
+    }
+
+    void ShowSettings()
+    {
+        if (settingsPanel != null)
+            settingsPanel.SetActive(true);
+    }
+
+    // متد برای بستن پنل (مثلاً به دکمه Close داخل پنل بدهید)
+    public void CloseSettings()
+    {
+        if (settingsPanel != null)
+            settingsPanel.SetActive(false);
+    }
+}
---
+++ Assets/work/SettingsTabManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class SettingsTabManager : MonoBehaviour
+{
+    [Header("Tab Buttons")]
+    public Button[] tabButtons;  
+
+    [Header("Content Pages")]
+    public GameObject[] tabPages; 
+
+    [Header("Default Tab Index")]
+    public int defaultTabIndex = 0; 
+
+    private int currentTabIndex = -1;
+
+    void Start()
+    {
+        
+        for (int i = 0; i < tabButtons.Length; i++)
+        {
+            int index = i; 
+            tabButtons[i].onClick.AddListener(() => SwitchTab(index));
+        }
+
+        
+        if (tabPages.Length > 0 && defaultTabIndex < tabPages.Length)
+        {
+            SwitchTab(defaultTabIndex);
+        }
+        else
+        {
+            HideAllPages();
+        }
+    }
+
+    public void SwitchTab(int tabIndex)
+    {
+        if (tabIndex < 0 || tabIndex >= tabPages.Length)
+            return;
+
+        if (currentTabIndex == tabIndex)
+            return; 
+
+      
+        HideAllPages();
+
+       
+        tabPages[tabIndex].SetActive(true);
+
+      
+        currentTabIndex = tabIndex;
+    }
+
+  
+    private void HideAllPages()
+    {
+        foreach (GameObject page in tabPages)
+        {
+            if (page != null)
+                page.SetActive(false);
+        }
+    }
+
+   
+    public int GetCurrentTabIndex()
+    {
+        return currentTabIndex;
+    }
+}
